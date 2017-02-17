a327c410cde28ab483cd2f2204d574f72068bf29
Miguel Zavala <miguelzavalac@gmail.com>
2017-02-16 19:07:43 -0600

    Corriendo test de db de prueba

--- test/db-test.js
+++ test/db-test.js
@@ -8,7 +8,7 @@ const r = require('rethinkdb')
 const DbName = `blog_${uuid.v4()}`
 const db = new Db({db: DbName})
 
-test.before('setup database', async function (t){
+test.before('setup database', async t => {
   await db.connect()
   t.true(db.connected, 'should by connected')
 })
@@ -19,10 +19,25 @@ test.after('disconnect database', async t => {
 })
 
 test.after.always('cleanup database', async t => {
-  let conn =  await r.connect({})
+  let conn = await r.connect({})
   await r.dbDrop(DbName).run(conn)
 })
 
 test('save image', async t => {
   t.is(typeof db.saveImage, 'function', 'saveImage is function')
+
+  let image = {
+    url: `https://blog.test/${uuid.v4()}.jpg`,
+    likes: 0,
+    liked: false,
+    user_id: uuid.uuid()
+  }
+
+  let created = await db.saveImage(image)
+  t.is(created.url, image.url)
+  t.is(created.likes, image.likes)
+  t.is(created.liked, image.liked)
+  t.is(created.user_id, image.user_id)
+  t.is(typeof created.id, 'string')
+  t.truthy(created.createdAt)
 })
